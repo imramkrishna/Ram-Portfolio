d638c65e558664d83d1d618bf18387f1acc7ec86
Ram Krishna Yadav <itsramky234@gmail.com>
2025-03-16 22:19:56 +0545

    Contact Section Modified

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base: '/', // Add this line
   build: {
     outDir: 'dist',
